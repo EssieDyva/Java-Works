a68caae6861a18d70dbb242bb9e0295e58e72013
EssieDyva <102621809+EssieDyva@users.noreply.github.com>
2025-05-15 13:24:39 +0200

    fixed classes

---
+++ 15-05-25/src/oop/Persona.java
@@ -0,0 +1,44 @@
+package oop;
+
+/**
+ * Classe che va a definire le caratteristiche di una persona
+ */
+public class Persona {
+    private String nome;
+    private String cognome;
+    private String genere;
+    private String indirizzo;
+    private String segnoZodiacale;
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCognome() {
+        return this.cognome;
+    }
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    public String getGenere() {
+        return this.genere;
+    }
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+    public String getIndirizzo() {
+        return this.indirizzo;
+    }
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+    public String getSegnoZodiacale() {
+        return this.segnoZodiacale;
+    }
+    public void setSegnoZodiacale(String segnoZodiacale) {
+        this.segnoZodiacale = segnoZodiacale;
+    }
+
+}
---
+++ 15-05-25/src/oop/PersonaMain.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class PersonaMain {
+    public static void main(String[] args) {
+        Persona personaProva = new Persona();
+        personaProva.setNome("Stefano");
+        personaProva.setCognome("De Siena");
+        personaProva.setGenere("Maschio");
+        personaProva.setIndirizzo("Via Roma 1");
+        personaProva.setSegnoZodiacale("Sagittario");
+
+    }
+    
+}
